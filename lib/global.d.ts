c9cb75c8a38693c0892894b505cf6abdfb7f2564
Oleg Mihailik <mihailik@gmail.com>
2025-02-13 18:07:20 +0000

    THREE.js rendering of firehose with balls

--- lib/global.d.ts
+++ lib/global.d.ts
@@ -2,3 +2,4 @@ declare var atproto_api: typeof import('./lib').atproto_api;
 declare var ipld_car: typeof import('./lib').ipld_car;
 declare var cbor_x: typeof import('./lib').cbor_x;
 declare var multiformats: typeof import('./lib').multiformats;
+declare var THREE: typeof import('three');
