c4ef5beb729268c077ec6b49dca9589e2c989b85
Oleg Mihailik <mihailik@gmail.com>
2025-02-13 18:07:19 +0000

    Both node and browser can handle firehose equaly.

--- lib/global.d.ts
+++ lib/global.d.ts
@@ -1,3 +1,4 @@
 declare var atproto_api: typeof import('./lib').atproto_api;
 declare var ipld_car: typeof import('./lib').ipld_car;
 declare var cbor_x: typeof import('./lib').cbor_x;
+declare var multiformats: typeof import('./lib').multiformats;
